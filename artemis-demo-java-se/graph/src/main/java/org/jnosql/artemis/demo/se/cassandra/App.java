6e58394a0b942b70834a6d31ec8f8621df2f5a53
Otavio Santana <otaviopolianasantana@gmail.com>
2017-08-19 16:27:40 -0300

    Fixes App

--- artemis-demo-java-se/graph/src/main/java/org/jnosql/artemis/demo/se/cassandra/App.java
+++ artemis-demo-java-se/graph/src/main/java/org/jnosql/artemis/demo/se/cassandra/App.java
@@ -16,16 +16,9 @@
 package org.jnosql.artemis.demo.se.cassandra;
 
 
-import org.jnosql.artemis.cassandra.column.CassandraTemplate;
-import org.jnosql.artemis.column.ColumnTemplate;
-import org.jnosql.diana.api.column.Column;
-import org.jnosql.diana.api.column.ColumnCondition;
-import org.jnosql.diana.api.column.ColumnQuery;
-
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
 import java.util.Arrays;
-import java.util.Optional;
 
 public class App {
 
@@ -38,15 +31,7 @@ public class App {
     public static void main(String[] args) {
 
         try(SeContainer container = SeContainerInitializer.newInstance().initialize()) {
-            ColumnTemplate columnTemplate =  container.select(CassandraTemplate.class).get();
-            Person saved = columnTemplate.insert(PERSON);
-            System.out.println("Person saved" + saved);
 
-            ColumnQuery query = ColumnQuery.of("Person");
-            query.and(ColumnCondition.eq(Column.of("id", 1L)));
-
-            Optional<Person> person = columnTemplate.singleResult(query);
-            System.out.println("Entity found: " + person);
 
         }
     }
