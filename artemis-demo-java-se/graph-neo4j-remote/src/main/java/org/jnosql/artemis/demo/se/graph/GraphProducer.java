28dec8008cec7180d47c887c6907e318c6e4c009
Otavio Santana <otaviopolianasantana@gmail.com>
2017-10-01 09:02:41 -0700

    Adds configuration resource

--- artemis-demo-java-se/graph-neo4j-remote/src/main/java/org/jnosql/artemis/demo/se/graph/GraphProducer.java
+++ artemis-demo-java-se/graph-neo4j-remote/src/main/java/org/jnosql/artemis/demo/se/graph/GraphProducer.java
@@ -18,33 +18,28 @@ import com.steelbridgelabs.oss.neo4j.structure.Neo4JElementIdProvider;
 import com.steelbridgelabs.oss.neo4j.structure.Neo4JGraph;
 import com.steelbridgelabs.oss.neo4j.structure.providers.Neo4JNativeElementIdProvider;
 import org.apache.tinkerpop.gremlin.structure.Graph;
-import org.neo4j.driver.v1.AuthToken;
-import org.neo4j.driver.v1.AuthTokens;
+import org.jnosql.artemis.ConfigurationUnit;
 import org.neo4j.driver.v1.Driver;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
-
-import static org.neo4j.driver.v1.GraphDatabase.driver;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class GraphProducer {
 
 
-    private static final String URL = "bolt://localhost:7687";
-    private static final String USER = "neo4j";
-    private static final String PASSWORD = "admin";
-    private static final AuthToken BASIC = AuthTokens.basic(USER, PASSWORD);
     private Neo4JGraph graph;
 
+    @Inject
+    @ConfigurationUnit
     private Driver driver;
 
 
     @PostConstruct
     public void init() {
-        driver = driver(URL, BASIC);
         Neo4JElementIdProvider<?> vertexIdProvider = new Neo4JNativeElementIdProvider();
         Neo4JElementIdProvider<?> edgeIdProvider = new Neo4JNativeElementIdProvider();
         this.graph = new Neo4JGraph(driver, vertexIdProvider, edgeIdProvider);
