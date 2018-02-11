81b21e22c76573ba1ddb2c66f61301b219457f33
Otavio Santana <otaviopolianasantana@gmail.com>
2018-02-11 21:12:28 -0200

    adds arango project

---
+++ artemis-demo-java-se/arangodb/src/main/java/org/jnosql/artemis/demo/se/couchbase/CouchbaseProducer.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2017 Otávio Santana and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ */
+package org.jnosql.artemis.demo.se.couchbase;
+
+import org.jnosql.artemis.ConfigurationUnit;
+import org.jnosql.diana.api.document.DocumentCollectionManager;
+import org.jnosql.diana.api.document.DocumentCollectionManagerFactory;
+import org.jnosql.diana.api.key.BucketManager;
+import org.jnosql.diana.api.key.BucketManagerFactory;
+import org.jnosql.diana.couchbase.document.CouchbaseDocumentCollectionManager;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Set;
+
+@ApplicationScoped
+public class CouchbaseProducer {
+
+    private static final String HEROES = "heroes";
+
+    @Inject
+    @ConfigurationUnit(name = "document")
+    private DocumentCollectionManagerFactory<CouchbaseDocumentCollectionManager> entityManager;
+
+    @Inject
+    @ConfigurationUnit(name = "key-value")
+    private BucketManagerFactory<BucketManager> bucketManager;
+
+
+    @Produces
+    public DocumentCollectionManager getManager() {
+        return entityManager.get(HEROES);
+    }
+
+    @Produces
+    public CouchbaseDocumentCollectionManager getCouchbaseDocumentCollectionManager() {
+        return entityManager.get(HEROES);
+    }
+
+    @Produces
+    public List<String> getHeroList() {
+        return bucketManager.getList(HEROES, String.class);
+    }
+
+
+    @Produces
+    public Set<String> getHeroSet() {
+        return bucketManager.getSet(HEROES, String.class);
+    }
+
+    @Produces
+    public BucketManager getBucketManager() {
+        return bucketManager.getBucketManager(HEROES);
+    }
+
+}
