7c1e7d39873ef147a03c05b1e4fb6973cac2c909
Otavio Santana <otaviopolianasantana@gmail.com>
2019-04-26 07:29:59 -0300

    adds validation

--- artemis-demo-java-se/bean-validation/src/main/java/org/jnosql/artemis/demo/se/parking/Car.java
+++ artemis-demo-java-se/bean-validation/src/main/java/org/jnosql/artemis/demo/se/parking/Car.java
@@ -16,6 +16,9 @@ package org.jnosql.artemis.demo.se.parking;
 
 import org.jnosql.artemis.Column;
 import org.jnosql.artemis.Entity;
+import org.jnosql.artemis.demo.se.parking.validation.CurrencyAccepted;
+import org.jnosql.artemis.demo.se.parking.validation.MonetaryMax;
+import org.jnosql.artemis.demo.se.parking.validation.MonetaryMin;
 
 import javax.money.MonetaryAmount;
 import javax.validation.constraints.NotBlank;
@@ -27,15 +30,85 @@ public class Car {
 
     @Column
     @NotNull
-    @Pattern(regexp = "")
+    @Pattern(regexp = "[A-Z]{3}-[0-9]{4}", message = "Invalid car plate")
     private String plate;
 
     @Column
     @NotNull
+    @MonetaryMin(value = "100", message = "There is not car cheap like that")
+    @MonetaryMax(value = "1000000", message = "The parking does not support fancy car")
+    @CurrencyAccepted(currencies = "USD", message = "The car price must work with USD")
     private MonetaryAmount price;
 
     @Column
     @NotBlank
     private String model;
+
+    private Car(String plate, MonetaryAmount price, String model) {
+        this.plate = plate;
+        this.price = price;
+        this.model = model;
+    }
+
+    @Deprecated
+    Car() {
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public MonetaryAmount getPrice() {
+        return price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "plate='" + plate + '\'' +
+                ", price=" + price +
+                ", model='" + model + '\'' +
+                '}';
+    }
+
+    public static CarBuilder builder() {
+        return new CarBuilder();
+    }
+
+    public static class CarBuilder {
+
+        private String plate;
+
+        private MonetaryAmount price;
+
+        private String model;
+
+        private CarBuilder() {
+        }
+
+        public CarBuilder withPlate(String plate) {
+            this.plate = plate;
+            return this;
+        }
+
+        public CarBuilder withPrice(MonetaryAmount price) {
+            this.price = price;
+            return this;
+        }
+
+        public CarBuilder withModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(plate, price, model);
+        }
+    }
+
 
 }
---
+++ artemis-demo-java-se/bean-validation/src/test/java/org/jnosql/artemis/demo/se/parking/CarTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2019 Otávio Santana and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ */
+package org.jnosql.artemis.demo.se.parking;
+
+import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import java.util.Locale;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    private static Validator validator;
+
+    private CurrencyUnit usd = Monetary.getCurrency(Locale.US);
+
+    @BeforeAll
+    public static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+
+    @Test
+    public void shouldReturnErrorWhenCarInvalidPlate() {
+        Car ferrari = Car.builder().withPlate("23-234")
+                .withModel("ferrari")
+                .withPrice(Money.of(1_0001, usd))
+                .build();
+
+        Set<ConstraintViolation<Car>> validate = validator.validate(ferrari);
+        Assertions.assertFalse(validate.isEmpty());
+        String message = validate.stream().findFirst()
+                .map(ConstraintViolation::getMessageTemplate)
+                .orElse(null);
+        assertEquals("Invalid car plate", message);
+
+    }
+
+
+    @Test
+    public void shouldReturnErrorWhenCarCheap() {
+        Car ferrari = Car.builder().withPlate("BRL-1234")
+                .withModel("ferrari")
+                .withPrice(Money.of(99, usd))
+                .build();
+
+        Set<ConstraintViolation<Car>> validate = validator.validate(ferrari);
+        Assertions.assertFalse(validate.isEmpty());
+        String message = validate.stream().findFirst()
+                .map(ConstraintViolation::getMessageTemplate)
+                .orElse(null);
+        assertEquals("There is not car cheap like that", message);
+    }
+
+
+    @Test
+    public void shouldReturnErrorWhenCarFancy() {
+        Car ferrari = Car.builder().withPlate("BRL-1234")
+                .withModel("ferrari")
+                .withPrice(Money.of(1_0000_000_000L, usd))
+                .build();
+
+        Set<ConstraintViolation<Car>> validate = validator.validate(ferrari);
+        Assertions.assertFalse(validate.isEmpty());
+        String message = validate.stream().findFirst()
+                .map(ConstraintViolation::getMessageTemplate)
+                .orElse(null);
+        assertEquals("The parking does not support fancy car", message);
+
+    }
+
+
+    @Test
+    public void shouldReturnErrorWhenCurrencyIsNotDollar() {
+
+        CurrencyUnit real = Monetary.getCurrency("BRL");
+
+        Car ferrari = Car.builder().withPlate("BRL-1234")
+                .withModel("ferrari")
+                .withPrice(Money.of(1000, real))
+                .build();
+
+
+        Set<ConstraintViolation<Car>> validate = validator.validate(ferrari);
+        Assertions.assertFalse(validate.isEmpty());
+        String message = validate.stream().findFirst()
+                .map(ConstraintViolation::getMessageTemplate)
+                .orElse(null);
+        assertEquals("The car price must work with USD", message);
+
+    }
+}
