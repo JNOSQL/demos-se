548557ef9649e06d222a7a63eae3b4ea7c7350b9
Otavio Santana <otaviopolianasantana@gmail.com>
2019-06-29 10:06:08 -0300

    updates package

---
+++ artemis-demo-java-se/couchbase/src/main/java/org/jnosql/artemis/demo/se/Villain.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2017 Otávio Santana and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ */
+package org.jnosql.artemis.demo.se;
+
+import org.jnosql.artemis.Column;
+import org.jnosql.artemis.Entity;
+import org.jnosql.artemis.Id;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Villain implements Serializable {
+
+    @Id
+    private String id;
+
+    @Column
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Villain villain = (Villain) o;
+        return Objects.equals(id, villain.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Villain{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ artemis-demo-java-se/mongodb/src/main/java/org/jnosql/artemis/demo/se/App2.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017 Otávio Santana and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ */
+
+package org.jnosql.artemis.demo.se;
+
+
+import org.jnosql.artemis.DatabaseQualifier;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class App2 {
+
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Long id = random.nextLong();
+
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+
+            Person person = Person.builder().
+                    withPhones(Arrays.asList("234", "432"))
+                    .withName("Name")
+                    .withId(id)
+                    .build();
+
+            PersonRepository repository = container.select(PersonRepository.class)
+                    .select(DatabaseQualifier.ofDocument()).get();
+            repository.save(person);
+
+            List<Person> people = repository.findByName("Name");
+            System.out.println("Entity found: " + people);
+            //repository.findByPhones("234").forEach(System.out::println);
+
+        }
+    }
+
+    private App2() {
+    }
+}
