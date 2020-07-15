9d3852450f5dbda616dd83e48fe7094f3b2bfc3f
otaviojava <otaviopolianasantana@gmail.com>
2020-07-15 10:29:20 -0300

    removes code

--- artemis-demo-java-se/mongodb-double/src/main/java/org/jnosql/artemis/demo/se/App.java
+++ artemis-demo-java-se/mongodb-double/src/main/java/org/jnosql/artemis/demo/se/App.java
@@ -36,24 +36,7 @@ public class App {
         Long id = random.nextLong();
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
 
-            Address address = new Address("Av nove de julho", "São Paulo");
-            Job job = new Job(12.12, "Developer");
-            Person person = Person.builder().
-                    withPhones(Arrays.asList("234", "432"))
-                    .withName("Name")
-                    .withAddress(address)
-                    .withJob(job)
-                    .withId(id).build();
-            DocumentTemplate template = container.select(DocumentTemplate.class).get();
-            Person saved = template.insert(person);
-            System.out.println("Person saved" + saved);
 
-
-            DocumentQuery query = select().from("Person")
-                    .where("_id").eq(id).build();
-
-            Optional<Person> personOptional = template.singleResult(query);
-            System.out.println("Entity found: " + personOptional);
 
         }
     }
