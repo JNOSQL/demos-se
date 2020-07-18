0d1b498cde33a6a2b328a17d948206d9bce0af99
otaviojava <otaviopolianasantana@gmail.com>
2020-07-18 09:05:58 -0300

    creates Service

--- artemis-demo-java-se/mongodb-double/src/main/java/org/jnosql/artemis/demo/se/Service.java
+++ artemis-demo-java-se/mongodb-double/src/main/java/org/jnosql/artemis/demo/se/Service.java
@@ -1,4 +1,20 @@
 package org.jnosql.artemis.demo.se;
 
+import jakarta.nosql.mapping.Database;
+import jakarta.nosql.mapping.DatabaseType;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
 public class Service {
+
+    @Inject
+    @Database(provider = "greek", value = DatabaseType.DOCUMENT)
+    private GodRepository greek;
+
+
+    @Inject
+    @Database(provider = "romain", value = DatabaseType.DOCUMENT)
+    private GodRepository romain;
 }
