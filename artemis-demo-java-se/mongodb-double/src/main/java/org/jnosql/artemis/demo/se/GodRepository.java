c17e7ee00eb6b28b4ae7442265c2b051242d5136
otaviojava <otaviopolianasantana@gmail.com>
2020-07-18 09:16:28 -0300

    fixes god repository method name

--- artemis-demo-java-se/mongodb-double/src/main/java/org/jnosql/artemis/demo/se/GodRepository.java
+++ artemis-demo-java-se/mongodb-double/src/main/java/org/jnosql/artemis/demo/se/GodRepository.java
@@ -3,7 +3,10 @@ package org.jnosql.artemis.demo.se;
 import jakarta.nosql.mapping.Repository;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public interface GodRepository extends Repository<God, String> {
+
+    List<God> findByName(String name);
 }
