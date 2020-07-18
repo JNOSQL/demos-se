c76c5bf7f264497dbdb481531ac9edf9d5272c76
otaviojava <otaviopolianasantana@gmail.com>
2020-07-18 09:05:44 -0300

    creates ManagerProducer

--- artemis-demo-java-se/mongodb-double/src/main/java/org/jnosql/artemis/demo/se/ManagerProducer.java
+++ artemis-demo-java-se/mongodb-double/src/main/java/org/jnosql/artemis/demo/se/ManagerProducer.java
@@ -1,4 +1,37 @@
 package org.jnosql.artemis.demo.se;
 
+import jakarta.nosql.document.DocumentCollectionManager;
+import jakarta.nosql.mapping.Database;
+import jakarta.nosql.mapping.DatabaseType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
+@ApplicationScoped
 public class ManagerProducer {
+
+    @Inject
+    @ConfigProperty(name = "db1")
+    private DocumentCollectionManager greek;
+
+
+    @Inject
+    @ConfigProperty(name = "db2")
+    private DocumentCollectionManager romain;
+
+    @ApplicationScoped
+    @Produces
+    @Database(provider = "greek", value = DatabaseType.DOCUMENT)
+    public DocumentCollectionManager getGreek() {
+        return greek;
+    }
+
+    @ApplicationScoped
+    @Produces
+    @Database(provider = "romain", value = DatabaseType.DOCUMENT)
+    public DocumentCollectionManager getRomain() {
+        return romain;
+    }
 }
