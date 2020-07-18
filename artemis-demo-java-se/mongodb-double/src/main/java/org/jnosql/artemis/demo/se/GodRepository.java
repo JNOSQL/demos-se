0f762ecad619772f3424c738fb7bfd34b4f6edf9
otaviojava <otaviopolianasantana@gmail.com>
2020-07-18 05:23:02 -0300

    defines GodRepository as application scoped

--- artemis-demo-java-se/mongodb-double/src/main/java/org/jnosql/artemis/demo/se/GodRepository.java
+++ artemis-demo-java-se/mongodb-double/src/main/java/org/jnosql/artemis/demo/se/GodRepository.java
@@ -2,5 +2,8 @@ package org.jnosql.artemis.demo.se;
 
 import jakarta.nosql.mapping.Repository;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public interface GodRepository extends Repository<God, String> {
 }
