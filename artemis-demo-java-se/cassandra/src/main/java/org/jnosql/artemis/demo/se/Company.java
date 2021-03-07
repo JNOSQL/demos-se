782503b8d21fca6f2e82c629d50ee55af42daaab
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-07 19:24:20 +0000

    updates company entity

--- artemis-demo-java-se/cassandra/src/main/java/org/jnosql/artemis/demo/se/Company.java
+++ artemis-demo-java-se/cassandra/src/main/java/org/jnosql/artemis/demo/se/Company.java
@@ -5,19 +5,20 @@ import jakarta.nosql.mapping.Entity;
 import jakarta.nosql.mapping.Id;
 import org.eclipse.jnosql.mapping.cassandra.column.UDT;
 
-import java.util.List;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
-public class Worker {
+public class Company {
 
     @Id
     private String nickname;
 
     @Column
     @UDT("money")
-    private Money salary;
+    private Money cost;
 
     @Column
     private Set<String> languages;
@@ -26,6 +27,63 @@ public class Worker {
     private Map<String, String> contacts;
 
     @Column
-    private List<> asdf;
+    @UDT("address")
+    private Set<Address> addresses;
 
+    public String getNickname() {
+        return nickname;
+    }
+
+    public Money getCost() {
+        return cost;
+    }
+
+    public Set<String> getLanguages() {
+        if (this.languages == null) {
+            return Collections.emptySet();
+        }
+        return Collections.unmodifiableSet(this.languages);
+    }
+
+    public Map<String, String> getContacts() {
+        if (this.contacts == null) {
+            return Collections.emptyMap();
+        }
+        return Collections.unmodifiableMap(contacts);
+    }
+
+    public Set<Address> getAddresses() {
+        if(this.addresses == null) {
+            return Collections.emptySet();
+        }
+        return Collections.unmodifiableSet(this.addresses);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Company company = (Company) o;
+        return Objects.equals(nickname, company.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nickname);
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "nickname='" + nickname + '\'' +
+                ", cost=" + cost +
+                ", languages=" + languages +
+                ", contacts=" + contacts +
+                ", addresses=" + addresses +
+                '}';
+    }
 }
