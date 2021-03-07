74dee5aba2e58b58d9956a1e666ab66d586026e5
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-07 19:10:14 +0000

    adds money equals, hascode and toString

--- artemis-demo-java-se/cassandra/src/main/java/org/jnosql/artemis/demo/se/Money.java
+++ artemis-demo-java-se/cassandra/src/main/java/org/jnosql/artemis/demo/se/Money.java
@@ -6,6 +6,7 @@ import jakarta.nosql.mapping.Entity;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.Objects;
 
 @Entity
 public class Money {
@@ -16,4 +17,49 @@ public class Money {
 
     @Column
     private BigDecimal amount;
+
+    @Deprecated
+    Money() {
+    }
+
+    private Money(Currency currency, BigDecimal amount) {
+        this.currency = currency;
+        this.amount = amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return Objects.equals(currency, money.currency) && Objects.equals(amount, money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currency, amount);
+    }
+
+    @Override
+    public String toString() {
+        return currency.getSymbol() + " "+ amount;
+    }
+
+    public Money of(Currency currency, BigDecimal amount) {
+        Objects.requireNonNull(currency, "currency is required");
+        Objects.requireNonNull(amount, "amount is required");
+        return new Money(currency, amount);
+    }
 }
