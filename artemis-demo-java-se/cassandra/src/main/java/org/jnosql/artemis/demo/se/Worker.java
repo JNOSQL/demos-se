43099e0fee0e1c8784fb4b596b60f2ed38fa2254
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-07 19:07:31 +0000

    creates wroter type

--- artemis-demo-java-se/cassandra/src/main/java/org/jnosql/artemis/demo/se/Worker.java
+++ artemis-demo-java-se/cassandra/src/main/java/org/jnosql/artemis/demo/se/Worker.java
@@ -1,2 +1,18 @@
-package org.jnosql.artemis.demo.se;public class Worker {
+package org.jnosql.artemis.demo.se;
+
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+import jakarta.nosql.mapping.Id;
+import org.eclipse.jnosql.mapping.cassandra.column.UDT;
+
+@Entity
+public class Worker {
+
+    @Id
+    private String nickname;
+
+    @Column
+    @UDT("money")
+    private Money salary;
+
 }
