a948de0855174468a98913efc1306deff268cfbf
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-07 19:07:25 +0000

    creates money converter

--- artemis-demo-java-se/cassandra/src/main/java/org/jnosql/artemis/demo/se/MoneyConverter.java
+++ artemis-demo-java-se/cassandra/src/main/java/org/jnosql/artemis/demo/se/MoneyConverter.java
@@ -2,5 +2,19 @@ package org.jnosql.artemis.demo.se;
 
 import jakarta.nosql.mapping.AttributeConverter;
 
-public class MoneyConverter implements AttributeConverter {
+import java.util.Currency;
+
+public class MoneyConverter implements AttributeConverter<Currency, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Currency attribute) {
+        return attribute.getSymbol();
+    }
+
+    @Override
+    public Currency convertToEntityAttribute(String dbData) {
+        return Currency.getAvailableCurrencies().stream()
+                .filter(c -> dbData.equals(c.getCurrencyCode()))
+                .findAny().orElse(null);
+    }
 }
