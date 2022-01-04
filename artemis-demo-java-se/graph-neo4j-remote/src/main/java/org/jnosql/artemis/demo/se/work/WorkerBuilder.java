083f8f691ea24b023798dd5f3486a256d236e8ce
Otavio Santana <otaviopolianasantana@gmail.com>
2022-01-04 18:40:30 +0000

    Create worker builder
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ artemis-demo-java-se/graph-neo4j-remote/src/main/java/org/jnosql/artemis/demo/se/work/WorkerBuilder.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021 Otávio Santana and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ */
+package org.jnosql.artemis.demo.se.work;
+
+import java.math.BigDecimal;
+
+public class WorkerBuilder {
+
+    private String name;
+    private String occupation;
+    private BigDecimal salary;
+
+    public WorkerBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public WorkerBuilder setOccupation(String occupation) {
+        this.occupation = occupation;
+        return this;
+    }
+
+    public WorkerBuilder setSalary(BigDecimal salary) {
+        this.salary = salary;
+        return this;
+    }
+
+    public Worker createWorker() {
+        return new Worker(name, occupation, salary);
+    }
+}
