3f7c9c810aa160cf3d1487ec46611bd66f811438
Otavio Santana <otaviopolianasantana@gmail.com>
2022-01-04 18:39:36 +0000

    Add worker to getter and setter in the method
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- artemis-demo-java-se/graph-neo4j-remote/src/main/java/org/jnosql/artemis/demo/se/work/Worker.java
+++ artemis-demo-java-se/graph-neo4j-remote/src/main/java/org/jnosql/artemis/demo/se/work/Worker.java
@@ -19,6 +19,7 @@ import jakarta.nosql.mapping.Entity;
 import jakarta.nosql.mapping.Id;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 public class Worker {
@@ -34,4 +35,47 @@ public class Worker {
 
     @Column
     private BigDecimal salary;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Worker worker = (Worker) o;
+        return Objects.equals(id, worker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", occupation='" + occupation + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
