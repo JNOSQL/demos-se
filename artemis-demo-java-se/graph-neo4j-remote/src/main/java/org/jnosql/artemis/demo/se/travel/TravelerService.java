e4744b8052f4fb0cf5347f11e28990b8f721353c
Otavio Santana <otaviopolianasantana@gmail.com>
2022-01-05 17:18:34 +0000

    Update travelerServive to add save method
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- artemis-demo-java-se/graph-neo4j-remote/src/main/java/org/jnosql/artemis/demo/se/travel/TravelerService.java
+++ artemis-demo-java-se/graph-neo4j-remote/src/main/java/org/jnosql/artemis/demo/se/travel/TravelerService.java
@@ -1,2 +1,38 @@
-package org.jnosql.artemis.demo.se.travel;public class TravelerService {
+/*
+ * Copyright (c) 2021 Otávio Santana and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ */
+package org.jnosql.artemis.demo.se.travel;
+
+import org.eclipse.jnosql.mapping.graph.GraphTemplate;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Optional;
+
+@ApplicationScoped
+class TravelerService {
+
+    @Inject
+    private GraphTemplate template;
+
+    public Optional<Traveler> findByName(String name) {
+        return template.getTraversalVertex().hasLabel("Traveler")
+                .has("name", name).next();
+    }
+
+    public Traveler save(Traveler traveler) {
+        return findByName(traveler.getName())
+                .orElseGet(() -> template.insert(traveler));
+    }
 }
