d4ff7598c85a9180be6ab7369681d4757ae8077b
Otavio Santana <otaviopolianasantana@gmail.com>
2022-01-05 15:55:47 +0000

    Add language as an entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ artemis-demo-java-se/graph-neo4j-remote/src/main/java/org/jnosql/artemis/demo/se/programmer/Language.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2021 Otávio Santana and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ */
+package org.jnosql.artemis.demo.se.programmer;
+
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+import jakarta.nosql.mapping.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Language {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String name;
+
+
+    /**
+     * @Deprecated
+     * It is only to the framework requirement.
+     */
+    Language() {
+    }
+
+    private Language(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Language language = (Language) o;
+        return Objects.equals(id, language.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Language{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public static Language of(String name) {
+        return new Language(name);
+    }
+}
