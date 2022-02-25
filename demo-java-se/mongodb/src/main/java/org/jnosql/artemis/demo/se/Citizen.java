ce9d000bec2a6aaf0650a638a322f6681ed03592
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-25 15:06:59 +0000

    Add Citzen entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- demo-java-se/mongodb/src/main/java/org/jnosql/artemis/demo/se/Citizen.java
+++ demo-java-se/mongodb/src/main/java/org/jnosql/artemis/demo/se/Citizen.java
@@ -1,2 +1,75 @@
-package org.jnosql.artemis.demo.se;public class Citizen {
+package org.jnosql.artemis.demo.se;
+
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+import jakarta.nosql.mapping.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Citizen {
+
+    @Id
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private City city;
+
+
+    Citizen() {
+    }
+
+    private Citizen(String id, String name, City city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Citizen citizen = (Citizen) o;
+        return Objects.equals(id, citizen.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Citizen{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", city=" + city +
+                '}';
+    }
+
+    public static Citizen of(String id, String name, City city){
+        Objects.requireNonNull(id, "id is required");
+        Objects.requireNonNull(name, "name is required");
+        Objects.requireNonNull(city, "city is required");
+        return new Citizen(id, name, city);
+    }
 }
