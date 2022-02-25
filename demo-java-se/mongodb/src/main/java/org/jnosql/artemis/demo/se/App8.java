3fbada15bd8850609550f576c48d2ff9cdf6b45e
Otavio Santana <otaviopolianasantana@gmail.com>
2022-02-25 15:01:38 +0000

    Create city sample
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ demo-java-se/mongodb/src/main/java/org/jnosql/artemis/demo/se/App8.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021 Otávio Santana and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ */
+package org.jnosql.artemis.demo.se;
+
+import jakarta.nosql.document.DocumentQuery;
+import jakarta.nosql.mapping.document.DocumentTemplate;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+import static jakarta.nosql.document.DocumentQuery.select;
+
+public class App7 {
+
+    public static void main(String[] args) {
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+
+            Map<String, Object> maps = Collections.singletonMap("1", "2");
+            UserScopePropertiesBroken entity1 = new UserScopePropertiesBroken("user", "scope", maps);
+
+            DocumentTemplate template = container.select(DocumentTemplate.class).get();
+
+          //  template.insert(entity1);
+
+            final DocumentQuery query1 = select().from("user_scope_properties_broken").where("_id")
+                    .eq("user").and("scope").eq("scope").build();
+            final Optional<Object> first1 = template.select(query1).findFirst();
+            System.out.println(first1);
+        }
+    }
+
+    private App7() {}
+}
