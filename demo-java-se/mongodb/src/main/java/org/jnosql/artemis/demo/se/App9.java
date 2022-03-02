5e6898db1c8cc2789a708e96c3d5ecffd1fe89d5
Otavio Santana <otaviopolianasantana@gmail.com>
2022-03-02 19:43:52 +0000

    Create scenario when there is null value
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ demo-java-se/mongodb/src/main/java/org/jnosql/artemis/demo/se/App9.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 Otávio Santana and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ */
+package org.jnosql.artemis.demo.se;
+
+import jakarta.nosql.document.DocumentQuery;
+import jakarta.nosql.mapping.document.DocumentTemplate;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+import java.util.Optional;
+
+import static jakarta.nosql.document.DocumentQuery.select;
+
+public class App9 {
+
+    public static void main(String[] args) {
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+
+            Citizen salvador = Citizen.of("1", "Salvador", null);
+
+            DocumentTemplate template = container.select(DocumentTemplate.class).get();
+
+            Citizen insert = template.insert(salvador);
+
+            final DocumentQuery query = select().from(Citizen.class.getSimpleName())
+                    .where("name").eq("Salvador").build();
+            final Optional<Citizen> citizen = template.singleResult(query);
+            System.out.println(citizen);
+
+            template.delete(Citizen.class, "1");
+        }
+    }
+
+    private App9() {
+    }
+}
