4f68f06c383df401d5a6f80824b1713c26680ccb
Otavio Santana <otaviopolianasantana@gmail.com>
2022-06-04 19:21:48 +0100

    Create Worker builder condition
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- demo-java-se/mongodb/src/main/java/org/jnosql/artemis/demo/se/WorkerBuilder.java
+++ demo-java-se/mongodb/src/main/java/org/jnosql/artemis/demo/se/WorkerBuilder.java
@@ -1,30 +1,12 @@
-/*
- * Copyright (c) 2022 Otávio Santana and others
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Apache License v2.0 which accompanies this distribution.
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
- *
- * You may elect to redistribute this code under either of these licenses.
- *
- * Contributors:
- *
- * Otavio Santana
- */
 package org.jnosql.artemis.demo.se;
 
 public class WorkerBuilder {
-
     private String id;
     private String name;
     private String city;
     private int age;
     private int dailyHours;
-
-    WorkerBuilder() {
-
-    }
+    private Gender gender;
 
     public WorkerBuilder id(String id) {
         this.id = id;
@@ -51,7 +33,12 @@ public class WorkerBuilder {
         return this;
     }
 
-    public Worker build() {
-        return new Worker(id, name, city, age, dailyHours);
+    public WorkerBuilder gender(Gender gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public Worker createWorker() {
+        return new Worker(id, name, city, age, dailyHours, gender);
     }
 }
