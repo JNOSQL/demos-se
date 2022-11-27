3a484bfa36b1015175b895809dc8547f99994fd9
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-27 07:27:58 +0000

    Update company
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cassandra/src/main/java/org/jnosql/demo/se/Company.java
+++ cassandra/src/main/java/org/jnosql/demo/se/Company.java
@@ -29,16 +29,6 @@ public class Company {
     private String name;
 
     @Column
-    @UDT("money")
-    private Money cost;
-
-    @Column
-    private Set<String> languages;
-
-    @Column
-    private Map<String, String> contacts;
-
-    @Column
     @UDT("headquarter")
     private Set<Headquarter> headquarters;
 
@@ -49,35 +39,13 @@ public class Company {
     Company() {
     }
 
-    private Company(String name, Money cost, Set<String> languages, Map<String, String> contacts,
-                    Set<Headquarter> headquarters) {
+    private Company(String name, Set<Headquarter> headquarters) {
         this.name = name;
-        this.cost = cost;
-        this.languages = languages;
-        this.contacts = contacts;
         this.headquarters = headquarters;
     }
 
     public String getName() {
         return name;
-    }
-
-    public Money getCost() {
-        return cost;
-    }
-
-    public Set<String> getLanguages() {
-        if (this.languages == null) {
-            return Collections.emptySet();
-        }
-        return Collections.unmodifiableSet(this.languages);
-    }
-
-    public Map<String, String> getContacts() {
-        if (this.contacts == null) {
-            return Collections.emptyMap();
-        }
-        return Collections.unmodifiableMap(contacts);
     }
 
     public Set<Headquarter> getAddresses() {
@@ -108,10 +76,7 @@ public class Company {
     public String toString() {
         return "Company{" +
                 "name='" + name + '\'' +
-                ", cost=" + cost +
-                ", languages=" + languages +
-                ", contacts=" + contacts +
-                ", addresses=" + headquarters +
+                ", headquarters=" + headquarters +
                 '}';
     }
 
@@ -123,45 +88,23 @@ public class Company {
     public static class CompanyBuilder {
         private String name;
 
-        private Money cost;
-
-        private Set<String> languages = new HashSet<>();
-
-        private Map<String, String> contacts = new HashMap<>();
-
         private Set<Headquarter> headquarters = new HashSet<>();
 
         private CompanyBuilder() {
         }
 
-        public CompanyBuilder withName(String name) {
+        public CompanyBuilder name(String name) {
             this.name = Objects.requireNonNull(name, "name is required");
             return this;
         }
 
-        public CompanyBuilder withCost(Money cost) {
-            this.cost = Objects.requireNonNull(cost, "cost is required");
-            return this;
-        }
-
-        public CompanyBuilder addLanguage(String language) {
-            this.languages.add(Objects.requireNonNull(language, "language is required"));
-            return this;
-        }
-
-        public CompanyBuilder add(String type, String contact) {
-            this.contacts.put(Objects.requireNonNull(type, "type is required"),
-                    Objects.requireNonNull(contact, "contact is required"));
-            return this;
-        }
-
-        public CompanyBuilder addHeadquarter(Headquarter headquarter) {
+        public CompanyBuilder add(Headquarter headquarter) {
             this.headquarters.add(Objects.requireNonNull(headquarter, "headquarter is required"));
             return this;
         }
 
         public Company build() {
-            return new Company(name, cost, languages, contacts, headquarters);
+            return new Company(name, headquarters);
         }
     }
 }
