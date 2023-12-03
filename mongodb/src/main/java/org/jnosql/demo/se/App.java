ddcecd6f8985da1879be465d002e6431cde7c771
Otavio Santana <otaviopolianasantana@gmail.com>
2023-12-03 07:29:29 +0000

    feat: update code
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mongodb/src/main/java/org/jnosql/demo/se/App.java
+++ mongodb/src/main/java/org/jnosql/demo/se/App.java
@@ -12,9 +12,12 @@
 package org.jnosql.demo.se;
 
 
+import jakarta.data.repository.Page;
+import jakarta.data.repository.Pageable;
+import jakarta.data.repository.Sort;
 import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
-import jakarta.nosql.document.DocumentTemplate;
+import net.datafaker.Faker;
 
 import java.util.Arrays;
 import java.util.Optional;
@@ -24,28 +27,21 @@ public class App {
 
 
     public static void main(String[] args) {
-
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-        long id = random.nextLong();
+        Faker faker = new Faker();
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+            BeerRepository repository = container.select(BeerRepository.class).get();
 
-            Address address = new Address("Av nove de julho", "São Paulo");
-            Job job = new Job(12.12, "Developer");
-            Person person = Person.builder().
-                    withPhones(Arrays.asList("234", "432"))
-                    .withName("Name")
-                    .withAddress(address)
-                    .withJob(job)
-                    .withId(id).build();
+            Pageable page = Pageable.ofPage(1).sortBy(Sort.desc("style"));
+            Page<Beer> page1 = repository.findAll(page);
+            System.out.println("The first page");
+            page1.forEach(System.out::println);
+            System.out.println("The second page");
+            Pageable secondPage = page.next();
+            Page<Beer> page2 = repository.findAll(secondPage);
+            page2.forEach(System.out::println);
 
-            DocumentTemplate template = container.select(DocumentTemplate.class).get();
-            Person saved = template.insert(person);
-            System.out.println("Person saved" + saved);
-
-            Optional<Person> personOptional = template.select(Process.class)
-                    .where("id").eq(id).singleResult();
-            System.out.println("Entity found: " + personOptional);
-
+            System.out.println("The query result: ");
+            repository.query().forEach(System.out::println);
         }
     }
 
--- mongodb/src/main/java/org/jnosql/demo/se/App2.java
+++ mongodb/src/main/java/org/jnosql/demo/se/App2.java
@@ -1,52 +1,15 @@
-/*
- * Copyright (c) 2022 Contributors to the Eclipse Foundation
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  and Apache License v2.0 which accompanies this distribution.
- *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
- *
- * You may elect to redistribute this code under either of these licenses.
- */
-
 package org.jnosql.demo.se;
 
-
-import org.eclipse.jnosql.mapping.DatabaseQualifier;
-
-import jakarta.enterprise.inject.se.SeContainer;
-import jakarta.enterprise.inject.se.SeContainerInitializer;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import jakarta.data.repository.Page;
+import jakarta.data.repository.Pageable;
 
 public class App2 {
 
-
     public static void main(String[] args) {
+        People people = null;
 
-        Random random = new Random();
-        long id = random.nextLong();
-
-        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
-
-            Person person = Person.builder().
-                    withPhones(Arrays.asList("234", "432"))
-                    .withName("Name")
-                    .withId(id)
-                    .build();
-
-            PersonRepository repository = container.select(PersonRepository.class)
-                    .select(DatabaseQualifier.ofDocument()).get();
-            repository.save(person);
-
-            List<Person> people = repository.findByName("Name");
-            System.out.println("Entity found: " + people);
-            //repository.findByPhones("234").forEach(System.out::println);
-
-        }
-    }
-
-    private App2() {
+        Page<Person> page = people.findAll(Pageable.ofPage(2).size(2));
+        Pageable nextPageable = page.nextPageable();
+        Page<Person> page2 = people.findAll(nextPageable);
     }
 }
