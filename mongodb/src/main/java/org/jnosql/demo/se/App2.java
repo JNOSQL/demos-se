ad8e5e1c40a33fbff9a7fd792a3899e7d7088744
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-24 05:43:32 +0000

    feat: update documentaion
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mongodb/src/main/java/org/jnosql/demo/se/App2.java
+++ mongodb/src/main/java/org/jnosql/demo/se/App2.java
@@ -1,15 +1,22 @@
 package org.jnosql.demo.se;
 
-import jakarta.data.repository.Page;
-import jakarta.data.repository.Pageable;
+
+import jakarta.data.page.Page;
+import jakarta.data.page.Pageable;
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
 
 public class App2 {
 
     public static void main(String[] args) {
-        People people = null;
 
-        Page<Person> page = people.findAll(Pageable.ofPage(2).size(2));
-        Pageable nextPageable = page.nextPageable();
-        Page<Person> page2 = people.findAll(nextPageable);
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+            People people = container.select(People.class).get();;
+
+            Page<Person> page = people.findAll(Pageable.ofPage(2).size(2));
+            Pageable nextPageable = page.nextPageable();
+            Page<Person> page2 = people.findAll(nextPageable);
+
+        }
     }
 }
