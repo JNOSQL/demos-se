8a5ca305898b0dd5c8ae4df7b66574675ff47420
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-24 08:22:21 +0100

    feat: update App6 to save and demonstrate multiple Avengers and Star Wars movies

--- arangodb/src/main/java/org/jnosql/demo/se/App6.java
+++ arangodb/src/main/java/org/jnosql/demo/se/App6.java
@@ -27,17 +27,30 @@ public class App6 {
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
 
             var repository = container.select(MovieRepository.class, DatabaseQualifier.ofDocument()).get();
-            var movie = repository.save(Movie.of("Avengers: Infinity War", 2018));
-            var wars = repository.save(Movie.of("Star Wars: The Last Jedi", 2017));
-            var star = repository.save(Movie.of("Star Trek Beyond", 2016));
-            var found = repository.findById(movie.id());
+            var avengers1 = repository.save(Movie.of("The Avengers", 2012));
+            var avengers2 = repository.save(Movie.of("Avengers: Age of Ultron", 2015));
+            var avengers3 = repository.save(Movie.of("Avengers: Infinity War", 2018));
+            var avengers4 = repository.save(Movie.of("Avengers: Endgame", 2019));
 
-            System.out.println(found);
+            var starWars1 = repository.save(Movie.of("Star Wars: A New Hope", 1977));
+            var starWars2 = repository.save(Movie.of("Star Wars: The Empire Strikes Back", 1980));
+            var starWars3 = repository.save(Movie.of("Star Wars: Return of the Jedi", 1983));
+            var starWars4 = repository.save(Movie.of("Star Wars: The Phantom Menace", 1999));
+            var starWars5 = repository.save(Movie.of("Star Wars: Attack of the Clones", 2002));
+            var starWars6 = repository.save(Movie.of("Star Wars: Revenge of the Sith", 2005));
+            var starWars7 = repository.save(Movie.of("Star Wars: The Force Awakens", 2015));
+            var starWars8 = repository.save(Movie.of("Star Wars: The Last Jedi", 2017));
+            var starWars9 = repository.save(Movie.of("Star Wars: The Rise of Skywalker", 2019));
+
             List<Movie> warMovies = repository.findByTitleContains("War");
-            System.out.println(warMovies);
+            List<Movie> startMovies = repository.findByTitleStartsWith("Star");
+            List<Movie> jediMovies = repository.findByTitleEndsWith("Jedi");
 
-            repository.deleteAll(List.of(movie, wars, star));
-            repository.deleteAll(repository.findAll().toList());
+            System.out.println("War Movies: " + warMovies);
+            System.out.println("Start Movies: " + startMovies);
+            System.out.println("Jedi Movies: " + jediMovies);
+            repository.deleteAll(List.of(avengers1, avengers2, avengers3, avengers4, starWars1, starWars2, starWars3, starWars4, starWars5, starWars6, starWars7, starWars8, starWars9));
+
 
         }
     }
