772ef37645e3c5e1d4744af6c2616f6deeb05170
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-24 07:50:12 +0100

    feat: add App6 class to demonstrate usage of MovieRepository with CDI

---
+++ arangodb/src/main/java/org/jnosql/demo/se/App6.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package org.jnosql.demo.se;
+
+
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
+import org.eclipse.jnosql.mapping.Database;
+import org.eclipse.jnosql.mapping.DatabaseQualifier;
+
+public class App6 {
+
+
+    public static void main(String[] args) {
+
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+
+            var repository = container.select(MovieRepository.class, DatabaseQualifier.ofDocument()).get();
+            var movie = repository.save(Movie.of("Avengers: Infinity War", 2018));
+
+            var found = repository.findById(movie.id());
+
+            System.out.println(found);
+
+        }
+    }
+
+    private App6() {
+    }
+}
