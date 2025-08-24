f22dee85b284535514958d2a909e6ea503a0021c
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-24 07:55:28 +0100

    feat: add methods to find movies by title patterns in MovieRepository

--- arangodb/src/main/java/org/jnosql/demo/se/MovieRepository.java
+++ arangodb/src/main/java/org/jnosql/demo/se/MovieRepository.java
@@ -3,6 +3,14 @@ package org.jnosql.demo.se;
 import jakarta.data.repository.CrudRepository;
 import jakarta.data.repository.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, String> {
+
+    List<Movie> findByTitleContains(String title);
+
+    List<Movie> findByTitleStartsWith(String title);
+
+    List<Movie> findByTitleEndsWith(String title);
 }
