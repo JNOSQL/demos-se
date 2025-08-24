4febda85715387682e5409061e5b8e31118b1e7f
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-24 07:55:22 +0100

    feat: enhance App6 to demonstrate saving, finding, and deleting movies in MovieRepository

--- arangodb/src/main/java/org/jnosql/demo/se/App6.java
+++ arangodb/src/main/java/org/jnosql/demo/se/App6.java
@@ -17,6 +17,8 @@ import jakarta.enterprise.inject.se.SeContainerInitializer;
 import org.eclipse.jnosql.mapping.Database;
 import org.eclipse.jnosql.mapping.DatabaseQualifier;
 
+import java.util.List;
+
 public class App6 {
 
 
@@ -26,10 +28,16 @@ public class App6 {
 
             var repository = container.select(MovieRepository.class, DatabaseQualifier.ofDocument()).get();
             var movie = repository.save(Movie.of("Avengers: Infinity War", 2018));
-
+            var wars = repository.save(Movie.of("Star Wars: The Last Jedi", 2017));
+            var star = repository.save(Movie.of("Star Trek Beyond", 2016));
             var found = repository.findById(movie.id());
 
             System.out.println(found);
+            List<Movie> warMovies = repository.findByTitleContains("War");
+            System.out.println(warMovies);
+
+            repository.deleteAll(List.of(movie, wars, star));
+            repository.deleteAll(repository.findAll().toList());
 
         }
     }
